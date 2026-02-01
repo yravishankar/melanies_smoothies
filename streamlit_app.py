4587cbc81753683dbc138e373c32f0307a7d5543
yravishankar <ravi.net.shankar@gmail.com>
2026-01-31 21:49:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,20 +18,20 @@ st.write('Current Movie Title is',title)
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be:', name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+# cnx = st.connection("snowflake")
+# session = cnx.session()
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# #st.dataframe(data=my_dataframe, use_container_width=True)
 
 
-ingredients_list = st.multiselect(
-    'Choose upto 5 ingredients:'
-    ,my_dataframe)
+# ingredients_list = st.multiselect(
+#     'Choose upto 5 ingredients:'
+#     ,my_dataframe)
 
-if ingredients_list: 
-  ingredients_string = ''
-  for fruit_chosen in ingredients_list:
-    ingredients_string += fruit_chosen + ' '
+# if ingredients_list: 
+#   ingredients_string = ''
+#   for fruit_chosen in ingredients_list:
+#     ingredients_string += fruit_chosen + ' '
 
 
 
