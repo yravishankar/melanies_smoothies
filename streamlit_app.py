894bf66f93f813b0c76145d84159cac31c035023
yravishankar <ravi.net.shankar@gmail.com>
2026-01-31 21:48:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,7 @@ if ingredients_list:
   ingredients_string = ''
   for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ' '
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
 
 # # st.write(ingredients_string)
@@ -48,5 +47,5 @@ if ingredients_list:
 # if time_to_insert:
 #     session.sql(my_insert_stmt).collect()
 #     st.success('Your Smoothie is ordered!', icon="✅")
-
-
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
