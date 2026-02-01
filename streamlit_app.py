1b3deb77c30186b0ae190a177c320e838622eaeb
yravishankar <ravi.net.shankar@gmail.com>
2026-01-31 21:36:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,4 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
